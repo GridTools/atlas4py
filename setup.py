83bf2d5504c45864632064cd6d305302333e5378
Enrique G. Paredes <enriqueg@cscs.ch>
2022-02-03 13:24:22 +0100

    Comment out debugging statement

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ class CMakeBuild(build_ext):
             "-DATLAS4PY_ATLAS_VERSION=" + VERSIONS["atlas"],
             "-DATLAS4PY_PYBIND11_VERSION=" + VERSIONS["pybind11"],
         ]
-        print(f"./{self.build_temp}$ " + " ".join(["cmake", ext.sourcedir] + cmake_args))
+        # print(f"./{self.build_temp}$ " + " ".join(["cmake", ext.sourcedir] + cmake_args))
         subprocess.check_call(["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp)
 
         # Run CMake build
