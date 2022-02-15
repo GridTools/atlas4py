c1277053cbf2c0e7e098b2c7a78d215e6f8cd266
Enrique G. Paredes <enriqueg@cscs.ch>
2022-02-15 14:41:33 +0100

    Missing quotes

--- check_version.py
+++ check_version.py
@@ -4,19 +4,16 @@ import re
 import requests
 
 
+pypi_version = requests.get("https://test.pypi.org/pypi/atlas4py/json").json()["info"]["version"]
+
 with open("src/atlas4py/_version.py", "r") as file:
     match = re.search("""__version__[ ]*=[ ]*[\'\"]([^ \'\"]+)""", file.read().replace("\n", " "))
     local_version = match[1].strip()
 
 
-pypi_version = requests.get("https://test.pypi.org/pypi/atlas4py/json").json()["info"]["version"]
-
-
 num_re = re.compile("[a-zA-Z]*([0-9]+)")
-
 local_parts = [int(num_re.match(p)[1]) for p in local_version.split(".")]
 pypi_parts = [int(num_re.match(p)[1]) for p in pypi_version.split(".")]
-
 
 if local_parts == pypi_parts or not all(l >= p for l, p in zip(local_parts, pypi_parts)):
     raise ValueError(
