bba1333d4abe06022dffaf10049c1bb4d555f32e
Enrique G. Paredes <enriqueg@cscs.ch>
2022-02-16 10:36:55 +0100

    Fixes

--- check_version.py
+++ check_version.py
@@ -10,7 +10,6 @@ with open("src/atlas4py/_version.py", "r") as file:
     match = re.search("""__version__[ ]*=[ ]*[\'\"]([^ \'\"]+)""", file.read().replace("\n", " "))
     local_version = match[1].strip()
 
-
 num_re = re.compile("[a-zA-Z]*([0-9]+)")
 local_parts = [int(num_re.match(p)[1]) for p in local_version.split(".")]
 pypi_parts = [int(num_re.match(p)[1]) for p in pypi_version.split(".")]
