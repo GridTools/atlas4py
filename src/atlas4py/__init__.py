3e1f9d725fbb345a6d9b649ebf6b578d68c478ec
Willem Deconinck <willem.deconinck@ecmwf.int>
2026-01-27 15:03:34 +0100

    Installation tweaks, robust to reinstallations and externalised atlas library

--- src/atlas4py/__init__.py
+++ src/atlas4py/__init__.py
@@ -5,8 +5,8 @@ Python bindings for the ECMWF Atlas mesh library.
 """
 import atexit
 
-from ._version import __version__
 from ._atlas4py import *
+from ._atlas4py import __version__
 
 _atlas4py._initialise()
 atexit.register(lambda: _atlas4py._finalise())
